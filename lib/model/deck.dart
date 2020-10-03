7ab68d208b30892e2325ebcd700e5d2adad0e21f
Riat Esteban <esriat0@gmail.com>
2020-10-03 19:59:30 +0200

    Added comments

--- lib/model/deck.dart
+++ lib/model/deck.dart
@@ -5,15 +5,11 @@ import 'package:flutter_hyperset/model/card_data/card.dart';
 import 'package:flutter_hyperset/model/card_data/shape.dart';
 import 'package:flutter_hyperset/model/card_data/colors.dart';
 
-///
 /// A deck contains a list of cards.
-///
 class Deck {
 	List<Card> deck = new List();
 	
-	///
 	/// This constructor takes a boolean as a parameter. If its value is true, we initialize the deck with all possible cards.
-	///
 	Deck(bool initializeCards){
 		if (initializeCards){
 			Shape.values.forEach((shape) {
@@ -27,18 +23,23 @@ class Deck {
 		}
 	}
 	
+	/// Adding a card to the deck
 	void addCard(Card card){
 		deck.add(card);
 	}
 	
+	/// Removing a card from the deck
 	void removeCard(Card card){
 		deck.remove(card);
 	}
 	
+	/// Shuffling the deck
 	void shuffle(){
 		deck.shuffle(new Random());
 	}
 	
+	/// Getting a slice of cards
+	/// Probably bugged. Will be tested soon
 	List<Card> getCards(int numberOfCards){
 		if (deck.length < numberOfCards){
 			throw new Exception("Not enough cards");
